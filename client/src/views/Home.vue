212728a2b58b4eb0ecc983110c08d35c84b4b07d
Marek Barak <mbarak@Mareks-MacBook-Pro.local>
2019-08-31 14:15:51 +0200

    Adding client

---
+++ client/src/views/Home.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    {{ thing }}
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import { mapState } from 'vuex'
+import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+
+@Component({
+  components: {
+    HelloWorld,
+  },
+  computed: {
+    ...mapState({
+      thing: 'thing'
+    })
+  }
+})
+export default class Home extends Vue {
+  thing: String;
+}
+</script>
